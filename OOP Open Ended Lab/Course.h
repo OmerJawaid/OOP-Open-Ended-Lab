3504de87c4963de5db239242d021f7892903c194
Muhammad Omer Jawaid <146324153+OmerJawaid@users.noreply.github.com>
2024-03-21 14:03:35 +0500

    updated Course remove student and add student

--- OOP Open Ended Lab/Course.h
+++ OOP Open Ended Lab/Course.h
@@ -5,6 +5,20 @@ class Course
     int courseCode;
     string courseName;
     public:
+        Teacher teacher;
     vector<Student>studentEnrolled;
-};
 
+    void addStudent(Student student) {
+		studentEnrolled.push_back(student);
+    };
+    void removeStudent(Student student) {
+        for (auto rem = studentEnrolled.begin(); rem != studentEnrolled.end(); ++rem)
+        {
+            if (rem->getstudentname() == student.getstudentname()) {
+                studentEnrolled.erase(rem);
+                return;
+            }
+    }
+		cout << "Student not found" << endl;
+    };
+};
