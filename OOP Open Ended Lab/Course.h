2cae00e956e86f58a83f3b1c1f194b29c439454f
Ahmad Iftikhar <ahmadiftikhar200536@gmail.com>
2024-03-21 14:03:30 +0500

    Course Class updates

--- OOP Open Ended Lab/Course.h
+++ OOP Open Ended Lab/Course.h
@@ -6,5 +6,22 @@ class Course
     string courseName;
     public:
     vector<Student>studentEnrolled;
+    Course(string name,int code) : courseName(name),courseCode(code){}
+   string getcoursename()
+   {
+    return courseName;
+   }
+   int getcoursecode()
+   {
+    return courseCode;
+   }
+
+   void viewStudents()
+   {
+    for(int i=0;i<studentEnrolled.size();i++)
+    {
+        cout<<studentEnrolled[i].getstudentname()<<endl;
+    }
+   }
 };
 
